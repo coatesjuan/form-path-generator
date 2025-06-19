58efb9e79cd5ad40a2733696c6bad8fb04853587
jcoates <juan.coates@combinedratio.com>
2025-06-19 19:22:30 -0300

    1 version of extension

--- extension.js
+++ extension.js
@@ -1,36 +1,148 @@
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
 const vscode = require('vscode');
 
-// This method is called when your extension is activated
-// Your extension is activated the very first time the command is executed
-
-/**
- * @param {vscode.ExtensionContext} context
- */
 function activate(context) {
 
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "form-path-generator" is now active!');
+    let disposable = vscode.commands.registerCommand('form-path-generator.FormPathGenerator', async function () {
+        const editor = vscode.window.activeTextEditor;
+        const document = editor.document;
 
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with  registerCommand
-	// The commandId parameter must match the command field in package.json
-	const disposable = vscode.commands.registerCommand('form-path-generator.helloWorld', function () {
-		// The code you place here will be executed every time your command is executed
+        if (editor) {
+            const selection = editor.selection;
+            let selectedAttribute = selection.isEmpty ? '' : document.getText(selection);
 
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from Form Path Generator!');
-	});
+            if (selectedAttribute) {
+                let selectionStartAt = selection.start;  // {c: 35, e: 13}
+                const selectionEndsAt = selection.end;    // {c: 35, e: 23}
 
-	context.subscriptions.push(disposable);
+                const range = new vscode.Range(selectionStartAt, selectionEndsAt);
+
+                // Get the document text copy
+                let documentText = document.getText();
+
+                // Edit json temporarily in memory: selectedAttribute -> :selectedAttribute
+                let modifiedText = documentText.substring(0, document.offsetAt(range.start)) + ":" + selectedAttribute + documentText.substring(document.offsetAt(range.end));
+
+                // Load json
+                let jsonObject = loadJsonObject(modifiedText);
+
+                // Find path for chosen attribute
+                await findSelectedAttributePaths(jsonObject, ":" + selectedAttribute);
+            } else {
+                let documentText = document.getText();
+                let jsonObject = loadJsonObject(documentText);
+                await promptForAttributeNameAndHandle(jsonObject);
+            }
+        }
+    });
+    
+    context.subscriptions.push(disposable);
 }
 
-// This method is called when your extension is deactivated
-function deactivate() {}
+
+// functions
+function findPaths(data, targetPhrase) {
+    let result = [];
+
+    function traverse(propAttribute, currPath) {
+        if (propAttribute.properties) {
+            for (const attribute in propAttribute.properties) {
+                const newPath = [...currPath, attribute];
+                traverse(propAttribute.properties[attribute], newPath);
+            }
+        } else if (propAttribute.items) {
+            const newPath = currPath;
+            traverse(propAttribute.items, newPath);
+        }
+
+        // Check if the target phrase is in the current path
+        if (currPath.includes(targetPhrase)) {
+            result.push(currPath);
+        }
+    }
+
+    traverse(data.Content, []);
+
+    result = [...new Set(result.reverse())]; // remove duplicates
+    return result;
+}
+
+async function findSelectedAttributePaths(jsonObject, selectedAttributeNewName) {
+    const paths = findPaths(jsonObject, selectedAttributeNewName);
+
+    if (paths.length === 0) {
+        selectedAttributeNewName = selectedAttributeNewName.replace(":", "");
+        vscode.window.showInformationMessage(`No attribute found with name: ${selectedAttributeNewName}`);
+    } else {
+        // remove ":" from path
+        let stringToCopy = arrayToString(paths[0]);
+        stringToCopy = stringToCopy.replace(/:/g, "");
+
+        // copy path to clipboard
+        await vscode.env.clipboard.writeText(stringToCopy);
+        vscode.window.showInformationMessage(`Copied to clipboard: ${stringToCopy}`);
+    }
+}
+
+async function promptForAttributeNameAndHandle(jsonObject) {
+    const attributeName = await vscode.window.showInputBox({
+        prompt: 'Enter attribute name'
+    });
+
+    if (attributeName) {
+        const paths = findPaths(jsonObject, attributeName);
+        if (paths.length === 0) {
+            vscode.window.showInformationMessage(`No attribute found with name: ${attributeName}`);
+        } else {
+            await displayPaths(paths);
+        }
+    }
+}
+
+async function displayPaths(paths) {
+    const pathOptions = paths.map(path => ({
+        label: arrayToString(path),
+        detail: `Click to copy: ${arrayToString(path)}`
+    }));
+
+    const selectedPath = await vscode.window.showQuickPick(pathOptions, {
+        placeHolder: 'Select path to copy',
+        matchOnDetail: true
+    });
+
+    if (selectedPath) {
+        await vscode.env.clipboard.writeText(selectedPath.label);
+        vscode.window.showInformationMessage(`Copied to clipboard: ${selectedPath.label}`);
+    }
+}
+
+function arrayToString(arr) {
+    // Capitalizar y quitar espacios para cada segmento
+    const formatSegment = (segment) => {
+        // separar por espacios
+        const words = segment.split(' ');
+        // capitalizar la primera letra de cada palabra
+        const capitalized = words.map(word => word.charAt(0).toUpperCase() + word.slice(1));
+        // unir sin espacios
+        return capitalized.join('');
+    };
+
+    const formattedSegments = arr.map(formatSegment);
+    return formattedSegments.join('_');
+}
+
+function loadJsonObject(jsonText) {
+    // Loading Json
+    try {
+        return JSON.parse(jsonText);
+    } catch (error) {
+        vscode.window.showErrorMessage('Error parsing JSON');
+        return;
+    }
+}
+
+function deactivate() { }
 
 module.exports = {
-	activate,
-	deactivate
-}
+    activate,
+    deactivate
+};
